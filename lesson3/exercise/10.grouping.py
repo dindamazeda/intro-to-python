20499fbde862d3e65281fed73a9dbb1ef7725a62
dindamazeda <d.mazeda@yahoo.com>
2020-09-07 23:33:48 +0800

    homework submission - revision 1

--- lesson3/exercise/10.grouping.py
+++ lesson3/exercise/10.grouping.py
@@ -6,25 +6,18 @@
 # program: ['mantra', 'marka', 'mleka', 'barka', 'deka', 'karma', 'koka', 'kvaka', 'ljorka', 'patka', 'seka', 'staka' ]
 
 words = ['patka', 'kvaka', 'staka', 'barka', 'marka', 'koka', 'ljorka', 'mantra', 'karma', 'deka', 'seka', 'mleka']
-# words.sort()
-# sorted_words = sorted(words)
-# print(sorted_words)
+sorted_words = sorted(words)
 
 m_words =[]
 no_m_words = []
-for x in words:
+for x in sorted_words:
     if x[0][0]=='m':
         m_words.append(x)
     else:
         no_m_words.append(x)
 
-sorted_m_words = sorted(m_words)
-sorted_no_m_words = sorted(no_m_words)
-print(m_words)
-print(no_m_words)
-
 final_words = []
-final_words.extend(sorted_m_words)
-final_words.extend(sorted_no_m_words)
+final_words.extend(m_words)
+final_words.extend(no_m_words)
 
 print(final_words)
