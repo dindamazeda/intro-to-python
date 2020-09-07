be2810b132471d21289c5a6f53b171258fa8a820
dindamazeda <d.mazeda@yahoo.com>
2020-09-07 23:24:53 +0800

    homework submission

--- lesson3/exercise/10.grouping.py
+++ lesson3/exercise/10.grouping.py
@@ -4,3 +4,27 @@
 ### example ###
 # words = ['patka', 'kvaka', 'staka', 'barka', 'marka', 'koka', 'ljorka', 'mantra', 'karma', 'deka', 'seka', 'mleka']
 # program: ['mantra', 'marka', 'mleka', 'barka', 'deka', 'karma', 'koka', 'kvaka', 'ljorka', 'patka', 'seka', 'staka' ]
+
+words = ['patka', 'kvaka', 'staka', 'barka', 'marka', 'koka', 'ljorka', 'mantra', 'karma', 'deka', 'seka', 'mleka']
+# words.sort()
+# sorted_words = sorted(words)
+# print(sorted_words)
+
+m_words =[]
+no_m_words = []
+for x in words:
+    if x[0][0]=='m':
+        m_words.append(x)
+    else:
+        no_m_words.append(x)
+
+sorted_m_words = sorted(m_words)
+sorted_no_m_words = sorted(no_m_words)
+print(m_words)
+print(no_m_words)
+
+final_words = []
+final_words.extend(sorted_m_words)
+final_words.extend(sorted_no_m_words)
+
+print(final_words)
