a238911a38e853524df67f4d7de0e5ea50db86e2
dindamazeda <d.mazeda@yahoo.com>
2020-09-09 17:55:27 +0800

    homework submission - revision 1

--- lesson3/exercise/9.smallest-largest.py
+++ lesson3/exercise/9.smallest-largest.py
@@ -2,8 +2,5 @@
 # list_of_number = [15, 116, 54, 30, 21, 41, 65, 7, 105, 135, 173, 56, 197, 56, 136, 83, 97, 130, 184, 112, 83, 157, 189, 24, 28, 127, 166, 99, 37, 114, 3, 123, 160, 97, 152]
 
 list_of_number = [15, 116, 54, 30, 21, 41, 65, 7, 105, 135, 173, 56, 197, 56, 136, 83, 97, 130, 184, 112, 83, 157, 189, 24, 28, 127, 166, 99, 37, 114, 3, 123, 160, 97, 152]
-list_of_number.sort()
-print(list_of_number)
-
-list_of_number.reverse()
-print(list_of_number)
+sorted_list = sorted(list_of_number)
+print('The smallest number is {} and the largest number is {}'. format(sorted_list[0],sorted_list[-1]))
