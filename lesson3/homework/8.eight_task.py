da09786dbbd60cb02d514d31ace3f578be5bc4a6
dindamazeda <d.mazeda@yahoo.com>
2020-09-09 21:35:05 +0800

    homework submission

---
+++ lesson3/homework/8.eight_task.py
@@ -0,0 +1,37 @@
+# OSMI ZADATAK
+# # Uz pomoć while petlje napisati program koji će da isprinta sledeću šemu
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+#
+# Eight Task
+# Using a while loop, write a program that will print the following scheme
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+
+
+star = '*'
+x = 1
+while x <=5 :
+    print('{}'.format(star * x))
+    x +=1
+
+y = 1
+while y <= 5:
+    print('{}'.format(star * (5-y)))
+    y +=1
+
+
